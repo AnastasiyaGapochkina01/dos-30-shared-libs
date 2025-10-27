49b10b60f97c18ee8e5eb1d0bb289b85f42112f0
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2025-10-27 20:37:01 +0300

    Update notifyTelegram.groovy

--- vars/notifyTelegram.groovy
+++ vars/notifyTelegram.groovy
@@ -1,7 +1,7 @@
 def call(String prefix, String botToken, String chatId) {
   def jobName = env.JOB_NAME ?: "Unknown job"
   def buildStatus = currentBuild.currentResult ?: "UNKNOWN"
-  
+  def buildUrl = env.BUILD_URL ?: "none"
 
 
   def tmpl = libraryResource 'telegramMessageTmpl.md'
