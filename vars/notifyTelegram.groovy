7b5aaa9c6fe8519757b5c7d39f4e621e28dd89d6
anestesia <anastasiya.gapochkina01@yandex.ru>
2025-10-27 16:25:40 +0000

    initial commit

---
+++ vars/notifyTelegram.groovy
@@ -0,0 +1,18 @@
+def call(String prefix, String botToken, String chatId) {
+  def jobName = env.JOB_NAME ?: "Unknown job"
+  def buildStatus = currentBuild.currentResult ?: "UNKNOWN"
+  def buildUrl = env.BUILD_URL ?: "none"
+
+
+  def tmpl = libraryResource 'telegramMessageTmpl.md'
+  def msg = tmpl
+        .replace('${jobName}', jobName)
+        .replace('${buildStatus}', buildStatus)
+        .replace('${buildUrl}', buildUrl)
+  sh """
+    curl -s -X POST https://api.telegram.org/bot${botToken}/sendMessage \
+      -d chat_id=${chatId} \
+      -d parse_mode=Markdown \
+      -d text="${msg.replace("\"", "\\\"")}"
+  """
+}
